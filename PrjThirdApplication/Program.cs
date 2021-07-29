9daf111f8e3a854bebf0194c33c4c24f79725206
=DhairyaC <=dhairyachheda5@gmail.com>
2021-07-29 12:53:04 +0530

    updated program.cs

--- PrjThirdApplication/Program.cs
+++ PrjThirdApplication/Program.cs
@@ -6,7 +6,7 @@ namespace PrjThirdApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
